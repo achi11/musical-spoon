9a9eb0248951a072f96eb7e75235c561c3c383d5
achiko tskabelia <achikotskabelia@gmail.com>
2018-07-12 21:19:23 +0400

    comit.

--- WindowsFormsApp4/Form1.Designer.cs
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -32,6 +32,12 @@
             this.btnDraw = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.btnClear = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -69,11 +75,69 @@
             this.btnClear.UseVisualStyleBackColor = true;
             this.btnClear.Click += new System.EventHandler(this.BtnClear);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(656, 462);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "swap";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(647, 398);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(647, 428);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(656, 366);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "btnGenerateNumbers";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(596, 401);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(596, 431);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "b";
+            // 
             // DrawForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(798, 548);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btnDraw);
@@ -91,6 +155,12 @@
         private System.Windows.Forms.Button btnDraw;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- WindowsFormsApp4/Form1.cs
+++ WindowsFormsApp4/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,23 +18,58 @@ namespace WindowsFormsApp4
             InitializeComponent();
         }
 
-        
+        List<Rectangle> reclist = new List<Rectangle>();
+        List<Color> colorlist = new List<Color>();
+        System.Drawing.SolidBrush fill;
         public void DrawRectangles()
         {
-            Pen red = new Pen(Color.Red);
-            Graphics g = groupBox1.CreateGraphics();           
+            Random random = new Random();
+            Graphics g = groupBox1.CreateGraphics();
             var arr = new int[Int32.Parse(textBox1.Text)];
             FillArray(arr);
             int rectcount = arr.Length;
             int distance = 30;
             int recwidth = (groupBox1.Width - (rectcount - 1) * distance) / rectcount - 2;
             int firstcoordinate = 0;
-            int secondcoordinate = 0;         
+            int secondcoordinate = 0;
             for (int i = 0; i < arr.Length; i++)
             {
+                Color color = Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
+                fill = new System.Drawing.SolidBrush(color);
                 secondcoordinate = groupBox1.Height - arr[i] - 5;
-                g.DrawRectangle(red, new Rectangle(firstcoordinate, secondcoordinate, recwidth, arr[i]));
+                var rect = new Rectangle(firstcoordinate, secondcoordinate, recwidth, arr[i]);
+                g.FillRectangle(fill, rect);
                 firstcoordinate += recwidth + 30;
+                if (reclist.Count < Int32.Parse(textBox1.Text))
+                {
+                    reclist.Add(rect);
+                    colorlist.Add(color);
+                }
+                else
+                {
+                    reclist[i] = rect;
+                    colorlist[i] = color;
+                }
+            }
+        }
+        public void SwapRectangle()
+        {
+            Graphics g = groupBox1.CreateGraphics();
+            int a = Int32.Parse(textBox2.Text);
+            int b = Int32.Parse(textBox3.Text);
+            if (reclist[a].Height < reclist[b].Height)
+            {
+                var c = new Rectangle(reclist[a].X, reclist[a].Y, reclist[a].Width, reclist[a].Height);
+                reclist[a] = new Rectangle(reclist[a].X, reclist[b].Y, reclist[a].Width, reclist[b].Height);
+                reclist[b] = new Rectangle(reclist[b].X, c.Y, reclist[b].Width, c.Height);
+                var k = colorlist[a];
+                colorlist[a] = colorlist[b];
+                colorlist[b] = k;
+            }
+            for (int i = 0; i < reclist.Count; i++)
+            {
+                fill = new System.Drawing.SolidBrush(colorlist[i]);
+                g.FillRectangle(fill, reclist[i]);
             }
         }
 
@@ -48,12 +84,26 @@ namespace WindowsFormsApp4
 
         private void BtnDraw(object sender, EventArgs e)
         {
+            groupBox1.Refresh();
             DrawRectangles();
         }
 
         private void BtnClear(object sender, EventArgs e)
         {
             groupBox1.Invalidate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            groupBox1.Refresh();
+            SwapRectangle();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            textBox2.Text = rnd.Next(Int32.Parse(textBox1.Text)).ToString();
+            textBox3.Text = rnd.Next(Int32.Parse(textBox1.Text)).ToString();
         }
     }
 }
